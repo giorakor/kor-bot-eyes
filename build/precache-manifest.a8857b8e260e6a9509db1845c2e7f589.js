5f83887ebda1e5335e1a9e62837018652927b452
Omri Kornblau <omri.354@gmail.com>
2020-04-16 17:43:08 +0300

    Uningnore 'build' folder

---
+++ build/precache-manifest.a8857b8e260e6a9509db1845c2e7f589.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecff001b2103690b9df7eb547fe53e3b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "551055299796fc0c87b9",
+    "url": "./static/css/main.3e3e7bd5.chunk.css"
+  },
+  {
+    "revision": "1aac8c7a8f36b21edc2c",
+    "url": "./static/js/2.a95426ed.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "./static/js/2.a95426ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551055299796fc0c87b9",
+    "url": "./static/js/main.64cfd8e1.chunk.js"
+  },
+  {
+    "revision": "8706ea448eba00b7ab0b",
+    "url": "./static/js/runtime-main.5452c733.js"
+  },
+  {
+    "revision": "882606798561421a2cebb1655df05593",
+    "url": "./static/media/eye-white.88260679.png"
+  },
+  {
+    "revision": "e1eb0a2868fd0a1fa3c8d9c40773eb08",
+    "url": "./static/media/pupil.e1eb0a28.png"
+  }
+]);
