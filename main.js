3dea7d1787a99b39ebce2dbd2c356c8e2cbe31ab
Omri Kornblau <omri.354@gmail.com>
2020-04-16 17:09:56 +0300

    Added electron

---
+++ main.js
@@ -0,0 +1,29 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.maximize();
+
+  mainWindow.loadFile('./build/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
